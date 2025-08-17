424a8d4e0743f9bdc725e4443a89419f65e6064d
dmextdev <exdev@mobile-affairs.com>
2025-08-17 21:11:16 +0300

    10.0.0.55
    fix(imports): Correct Sherlock library import path

--- src/chimera_intel/core/social_osint.py
+++ src/chimera_intel/core/social_osint.py
@@ -2,10 +2,9 @@
 # Sherlock is a powerful library, but it's designed for command-line use.
 # We need to import its core components and adapt them for our library.
 
-# --- ФИНАЛНА КОРЕКЦИЯ ТУК ---
 
-from sherlock.sherlock import sherlock
-from sherlock import sites
+from sherlock.sherlock import sherlock  # type: ignore
+from sherlock import sites  # type: ignore
 from .schemas import SocialProfile, SocialOSINTResult
 from .utils import save_or_print_results
 from .database import save_scan_to_db
