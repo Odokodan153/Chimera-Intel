bcc918b7f6110b91f1ac10d4bd083c46bd8ad6d6
dmextdev <exdev@mobile-affairs.com>
2025-10-14 13:02:58 +0300

    Update tests

--- Tests/test_page_monitor.py
+++ Tests/test_page_monitor.py
@@ -1,5 +1,5 @@
 from typer.testing import CliRunner
-from unittest.mock import patch, MagicMock, mock_open
+from unittest.mock import patch, MagicMock
 import asyncio
 
 # The application instance to be tested
